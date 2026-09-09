557288523443ffd890046c765d9caf32c4781b2f
baloiben902-stack <baloiben902@gmail.com>
2026-09-09 22:52:25 +0000

    Add Firebase authentication and user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,36 @@
+class TradiverseUser {
+      final String id;
+        final String fullName;
+          final String email;
+            final String phoneNumber;
+              final String profileImage;
+                final String location;
+                  final String country;
+                    final String bio;
+                      final List<String> skills;
+                        final List<String> interests;
+                          final double trustScore;
+                            final int completedTrades;
+                              final double rating;
+                                final DateTime? createdAt;
+                                  final String accountStatus;
+
+                                    const TradiverseUser({
+                                        required this.id,
+                                            required this.fullName,
+                                                required this.email,
+                                                    required this.phoneNumber,
+                                                        required this.profileImage,
+                                                            required this.location,
+                                                                required this.country,
+                                                                    required this.bio,
+                                                                        required this.skills,
+                                                                            required this.interests,
+                                                                                required this.trustScore,
+                                                                                    required this.completedTrades,
+                                                                                        required this.rating,
+                                                                                            required this.createdAt,
+                                                                                                required this.accountStatus,
+                                                                                                  });
+                                                                                                  
+}
